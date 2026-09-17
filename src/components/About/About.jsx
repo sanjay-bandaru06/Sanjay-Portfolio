559997cef6b25e09a98c796970e9bae859fcea3c
Sanjay Bandaru <sanjay.bandaru565@gmail.com>
2026-09-17 19:16:00 +0530

    Fix profile image path

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/profile.png';
+import profileImage from '../../assets/Profile.png';
 
 const About = () => {
   return (
